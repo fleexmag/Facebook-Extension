894a303c52b169810870970dd6357c140affe291
Artem <smurfik997@gmail.com>
2017-07-13 13:28:49 +0300

    Commit

---
+++ js/content_script_v2.56.js
@@ -0,0 +1,223 @@
+'use strict';
+
+//function for get storage data
+function storageGet(name, callback)  {
+    chrome.storage.local.get(name, callback);
+}
+
+//function for set storage data 
+function storageSet(data) {
+    chrome.storage.local.set(data);
+}   
+
+//function for get url to extension source
+function getURL(source) {
+    return chrome.extension.getURL(source);
+}
+
+//function for get message 
+function getMessage(name) {
+    return chrome.i18n.getMessage(name);
+}
+
+//function for get blocks 
+function doc(name, num) {
+    var res;
+
+    if (num != undefined) {
+        res = document.getElementsByClassName(name)[num];
+    } else {
+        res = document.getElementById(name);
+    }
+
+    return res;
+}
+
+//function for generate block with content for right col
+function genBlock(content, styles) {
+    var classesNames = ['pagelet-group pagelet', '_1-ia', '_4-u2 _1-ib _2tyk _20os _4-u8'];
+    var displayGroup = 'rhc';
+    var id = 'pagelet_i_want_to_believe_' + displayGroup;
+    
+    var res = '<div class="'+classesNames[0]+'" id="'+id+'" data-referrer="'+id+'" data-display-group="'+displayGroup+'"><div class="'+classesNames[1]+'"><div class="'+classesNames[2]+'" style="'+styles+'">'+content+'</div></div></div>';
+
+    return res;
+}
+
+//global vars
+var imageSrc = getURL('images/image.png');
+var scrollUpButtonSrc = getURL('images/up.png');
+
+//function for add block to rhc
+function addBlockToRHC(name, num) {
+    var res;
+
+    storageGet('addPhoto', function(storage) {
+        if (doc(name, num) != undefined && doc('_2t-a _26aw _5rmj _50ti _2s1y', 0)) {
+            if (storage.addPhoto == 1) {
+                //add block with photo 'I want to believe
+                var width = '284px';
+                var height = '355px';
+                var styles = 'cursor: pointer;';
+
+                doc(name, num).innerHTML = genBlock('<img src="'+imageSrc+'" title="'+getMessage('hide')+'" width="'+width+'" height="'+height+'"/>', styles) + doc(name, num).innerHTML;
+                doc('pagelet_i_want_to_believe_rhc').onclick = function() {
+                    storageSet({'addPhoto': '0'});
+                    location.reload();
+                };  
+            } else {
+                //add block with button 'Show'
+                var styles = 'height: 20px; width: 284px; background-color: '+doc('_2t-a _26aw _5rmj _50ti _2s1y', 0).style.backgroundColor+'; font: 22px sans-serif; color: white; text-align: center; line-height: 20px; cursor: pointer;';
+
+                doc(name, num).innerHTML = genBlock(getMessage('show'), styles) + doc(name, num).innerHTML;
+                doc('pagelet_i_want_to_believe_rhc').onclick = function() {
+                    storageSet({'addPhoto': '1'});
+                    location.reload();
+                };  
+            }
+
+            res = 'success';
+        }
+    });
+
+    return res;
+}
+
+//function for animate 
+function animate(func, duration) {
+    var start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        var timePassed = time - start;
+
+        if (timePassed >= duration) {
+            timePassed = duration;
+        }
+
+        func(timePassed);
+
+        if (timePassed < duration) {
+            requestAnimationFrame(animate);
+        }
+    });
+}
+
+//function for page scroll
+function onScroll() {
+    var scrollValue = window.pageYOffset;
+
+    if (scrollValue >= 800 && doc('up_button_block') != undefined) {
+        doc('up_button_block').style.display = 'block';
+        
+        if (doc('up_button_block').style.opacity == 0) {
+            var time = 1000;
+            var plus = 1/time;
+
+            animate(function(timePassed) {
+                doc('up_button_block').style.opacity = plus * timePassed;
+            }, time);
+        }
+    }  else if (doc('up_button_block') != undefined) {
+        doc('up_button_block').style.display = 'none';
+        doc('up_button_block').style.opacity = '0';
+    }
+}
+
+//change facebook theme color ------------ beta ------------
+storageGet('color', function(storage) {
+    var classesNames = [
+        '_2t-a _26aw _5rmj _50ti _2s1y',
+        '_2n_9',
+        '_59fb _tmz',
+        '_5lxt'
+    ];
+
+    var colors = {
+        '1': 'rgb(19, 207, 19)',
+        '2': 'rgb(255, 195, 0)',
+        '3': 'rgb(250, 60, 76)',
+        'standart': '#3b5998'
+    }
+
+    if (storage.color != 'standart' && storage.color != undefined && doc(classesNames[0], 0) != undefined) {
+        doc(classesNames[0], 0).style.backgroundColor = colors[storage.color];
+        doc(classesNames[0], 0).style.borderBottom = colors[storage.color];
+        doc(classesNames[1], 0).style.backgroundImage = 'url('+getURL('images/colors/color' + storage.color +'.1.png')+')';
+        doc(classesNames[1], 1).style.backgroundImage = 'url('+getURL('images/colors/color' + storage.color +'.1.png')+')';
+        doc(classesNames[1], 2).style.backgroundImage = 'url('+getURL('images/colors/color' + storage.color +'.1.png')+')';
+        doc(classesNames[2], 0).style.backgroundImage = 'url('+getURL('images/colors/color' + storage.color +'.1.png')+')';
+        doc(classesNames[3], 0).style.backgroundImage = 'url('+getURL('images/colors/color' + storage.color +'.1.png')+')';
+    } else if (doc(classesNames[0], 0) != undefined) {
+        doc(classesNames[0], 0).style.backgroundColor = colors['standart'];
+        doc(classesNames[0], 0).style.borderBottom = colors['standart'];
+    }
+});
+
+//------------ end of beta ------------
+
+storageGet('page_url', function(storage) {
+    if (storage.page_url != location.href && location.href.split('?')[0].split('www.facebook.com')[1] != '/photo.php' && location.href.search('/photos/') == 0) {
+        storageSet({'page_url': location.href});
+        location.reload();
+    } else {
+        //remove right col and chat etc.
+        if (doc('facebook') != undefined && doc('globalContainer') != undefined) {
+            if (doc('facebook').classList.contains('sidebarMode') != 0)
+            {
+                doc('facebook').classList.remove('sidebarMode');
+            }
+
+            doc('globalContainer').style.paddingRight = '0px';
+        }
+
+        if (doc('fbDockChatBuddylistNub') != undefined) {
+            doc('fbDockChatBuddylistNub').style.display = 'none';
+        }
+
+        //remove pagelet_ego_pane
+        if (doc('pagelet_ego_pane') != undefined) {
+            [].forEach.call(doc('pagelet_ego_pane').getElementsByTagName('*'), function(value, num) {
+                doc('pagelet_ego_pane').getElementsByTagName('*')[num].style.display = 'none';
+            });
+            
+            doc('pagelet_ego_pane').id = 'pagelet_ego_pane_with_photo';
+            doc('pagelet_ego_pane_with_photo').setAttribute('data-referrer', 'pagelet_ego_pane_with_photo');
+
+            addBlockToRHC('pagelet_ego_pane_with_photo');
+        }
+
+        //params of changes for pages: "profile" or another pages 
+        if (doc('fbProfileCover') != undefined && doc('globalContainer') != undefined) {
+            //new window size for profile page
+            doc('globalContainer').style.width = '851px';
+        } else if (doc('globalContainer') != undefined) {
+            doc('globalContainer').style.width = '';
+        }
+
+        //add scroll up button 
+        if (doc('up_button_block') == undefined && doc('pagelet_sidebar') != undefined && doc('_2t-a _26aw _5rmj _50ti _2s1y', 0) != undefined) {
+            doc('pagelet_sidebar').id = 'scroll_up_button';
+            doc('scroll_up_button').setAttribute('data-referrer', 'scroll_up_button');
+
+            var styles = 'position: fixed; width: 31px; height: 31px; bottom: 25px; left: 25px; border-radius: 100%; display: none; opacity: 0; cursor: pointer; background-color: '+doc('_2t-a _26aw _5rmj _50ti _2s1y', 0).style.backgroundColor+';';
+            var id = 'up_button_block';
+            var width = '31px';
+            var height = '31px';
+
+            doc('scroll_up_button').innerHTML = '<div id="'+id+'" style="'+styles+'"><img src="'+scrollUpButtonSrc+'" width="'+width+'" height="'+height+'"/></div>';
+            doc('up_button_block').onclick = function() {
+                var time = 500;
+                var scrollValue = window.pageYOffset;
+
+                animate(function(timePassed) {
+                    window.scroll(window.pageXOffset, scrollValue - (scrollValue/time) * timePassed);
+                }, time);
+            };
+        }
+
+        //onscroll event
+        window.onscroll = function() {
+            onScroll();
+        };
+    }
+});
--- js/popup.js
+++ js/popup.js
@@ -1,111 +1,101 @@
-//native function of getting block
+//function for get block by id
 function doc(id) {
-    var res = document.getElementById(id);
-    return res;
+    return document.getElementById(id);
 }
 
-//function for changing icon
-function icon(status) {
-    if (status == 'enabled')
-    {
-        chrome.browserAction.setIcon({
-    	    path : {
-		        "128": "images/icon128.png"
-	        }
-        });
-    } else if (status == 'disabled') {
-	    chrome.browserAction.setIcon({
-    	    path : {
-        	    "128": "images/icon128_disabled.png"
-    	    }
-        });
-    }
+//function for reload 
+function reload() {
+    chrome.tabs.reload();
+    location.reload();
 }
 
-//function for button
-function click() {
+//function for change extension icon
+function changeIcon(status) {
+    chrome.browserAction.setIcon({
+        path: {
+            '128': 'images/icon128_' + status + '.png'
+        }
+    });
+}
+
+//function for enable button
+function enableButtonClicked() {
     storageGet('enabled', function(storage) {
         if (storage.enabled != '0') {
-            storageSet('enabled', '0');
-            icon('disabled');
+            storageSet({'enabled': '0'});
+            changeIcon('disabled');
         } else {
-            storageSet('enabled', '1');
-            icon('enabled');  
+            storageSet({'enabled': '1'}); 
+            changeIcon('enabled');
         }
     });
 
-    chrome.tabs.reload();
-    location.reload();
+    reload();
 }
 
-//function for select color
-function click_sel(num) {
-    num = num.split('color')[1];
+//function for change selected color
+function changeColor(color) {
+    color = color.split('color')[1];
 
-    storageGet('color', function (storage) {
-        if (num == undefined) {
-            storageSet('color', 'standart');
+    storageGet('color', function(storage) {
+        if (color == undefined) {
+            storageSet({'color': 'standart'});
         } else {
-            storageSet('color', num);
+            storageSet({'color': color});
         }
     });
 
-    chrome.tabs.reload();
-    location.reload();
+    reload();
 }
 
-var count = 3; //colors count
+//function for configurate color buttons
+function buttonsConf() {
+    var count = {'hor': 3, 'ver': 1}; 
+    var size = (doc('b1').clientWidth - 10 * count['hor'])/count['hor'];
 
-//function for autoresize color buttons
-function buttons_autoresize() {
-    var size = (doc('b1').clientWidth - 10 * count)/count;
+    for(n = 1; n <= count['hor']; n++) {
+        doc('color' + n).style.width = size;
+        doc('color' + n).style.height = size;
+        doc('selected' + n).style.display = 'none';
 
-    for (i = 1; i <= count; i++) {
-        doc('color' + i).style.width = size;
-        doc('color' + i).style.height = size;
-        doc('selected' + i).style.height = size;
-        doc('selected' + i).style.width = size;
+        doc('color' + n).onclick = function(e) {
+            changeColor(e.target.id);
+        };
+
+        doc('selected' + n).style.width = size;
+        doc('selected' + n).style.height = size;
     }
 
-    doc('colors').style.width = size * count + 10 * count;
-    doc('colors').style.height = size + 10;
+    doc('colors').style.width = size * count['hor'] + 10 * count['hor'];
+    doc('colors').style.height = size * count['ver'] + 10 * count['ver'];
+
+    storageGet('color', function(storage) {
+        if (doc('selected' + storage.color) != undefined) {
+            doc('selected' + storage.color).style.display = 'block';
+        }
+    });
 }
 
-doc('b1').onclick = function() {
-    click();
-};
-
-for (i = 1; i <= count; i++)
-{
-    doc('color' + i).onclick = function(event) {
-        click_sel(event.target.id);
-    }
-}
-
-doc('version').innerHTML = 'v'+chrome.app.getDetails().version;
-
-storageGet(['enabled', 'color'], function(storage) {
+//main 
+storageGet('enabled', function(storage) {
+    //first stage
     if (storage.enabled != '0') {
-        doc('b1').innerHTML = chrome.i18n.getMessage('button_a');
+        doc('b1').innerHTML = getMessage('button_a');
         doc('b1').classList = 'b1';
-        icon('enabled');
+        changeIcon('enabled');
     } else {
-        doc('b1').innerHTML = chrome.i18n.getMessage('button_d');
+        doc('b1').innerHTML = getMessage('button_d');
         doc('b1').classList = 'b1 d';
-        icon('disabled');
-    }
-    buttons_autoresize();
-
-    //display selected color
-    for (i = 1; i <= count; i++) {
-        doc('selected' + i).style.display = 'none';
+        changeIcon('disabled');
     }
 
-    if (storage.color == 1) {
-        doc('selected1').style.display = 'block';
-    } else if (storage.color == 2) {
-        doc('selected2').style.display = 'block';
-    } else if (storage.color == 3) {
-        doc('selected3').style.display = 'block';
-    }
+    doc('b1').onclick = function() {
+        enableButtonClicked();
+    };
+
+    //second stage 
+    buttonsConf();
+
+    //third stage 
+    doc('version').innerHTML = 'v' + chrome.app.getDetails().version;
 });
